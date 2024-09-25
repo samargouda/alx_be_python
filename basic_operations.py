fd33084337440d7d5df1063859d9b040d000d61f
samar <samar.goda810@gmail.com>
2024-09-26 02:44:23 +0300

    Add files via upload

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10 
+number2 = 5
+sum = number1 + number2
+subtraction = number1 - number2
+multiplcation = number1 * number2
+print ("Addition of" , number1 , "and" , number2 ,"is" , sum)
+print ("subtraction of " , number1 , "and" , number2 , "is" , subtraction)
+print ("multipication of" , number1 , "and" , number2 , "is" , multiplcation)
